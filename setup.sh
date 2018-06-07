db368b584f28e7228256eb0944af99ab1e5f59ec
Jan Hanke <haja1023@hs-karlsruhe.de>
2018-06-07 07:26:09 +0000

    Add setup.sh draft

---
+++ setup.sh
@@ -0,0 +1,32 @@
+# Setup Script Draft
+
+# Create user
+# Set password
+# Add user to sudo group
+
+# Change to user
+
+# Update and upgrade apt
+
+# Install necessary programs
+## zsh
+## zsh-syntax-highlighting
+## bash-completion
+## vim
+## git
+## htop
+## tmux
+## ranger
+## powerline
+## python
+## g++
+## make
+## radare2 (Manual installation)
+## gdb
+## nmap
+
+# Setup git
+## user.email
+## user.name
+
+# Clone dotfiles and copy everything into place
