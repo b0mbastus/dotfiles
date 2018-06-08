7c8abca0108624ee3da56086d1b432ff5056568c
JanHanke <haja1023@hs-karlsruhe.de>
2018-06-08 20:20:27 +0200

    More home-path fiddeling

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,13 +7,11 @@ SCRIPTDIR=$(dirname "$SCRIPT")
 while true; do
 	read -p 'Do you want to add an ssh-key? ' yn
 	case $yn in
-		[Yy]* ) mkdir $HOME/.ssh
-				### Ask for ssh-public-key
-				read -p 'Enter a filename for the key: ' filename
-				vi $HOME/.ssh/${filename}
-				### Insert public key into athorized
-				$HOME/.ssh/${filename} > $HOME/.ssh/authorized_keys
-				break;;
+		[Yy]* ) mkdir ${HOME}/.ssh
+			read -p 'Enter a filename for the key: ' filename
+			vi ${HOME}/.ssh/${filename}
+			${HOME}/.ssh/${filename} > ${HOME}/.ssh/authorized_keys
+			break;;
 		[Nn]* ) break;;
 		* ) echo "Please answer yes or no.";;
 	esac
@@ -38,24 +36,23 @@ git config --global user.name $name
 git config --global core.editor 'vim'
 
 #Clone dotfile repo and checkout correct branch
-DOTFILES=$HOME/.dotfiles
-git clone https://github.com/JanHanke/config_files.git ${DOTFILES}
-cd ${DOTFILES}
+
+git clone https://github.com/JanHanke/config_files.git ${HOME}/.dotfiles
+cd ${HOME}/.dotfiles
 git checkout virtual
 
 # Make directory for manually updated packages and install them
-mkdir $HOME/packages
-cd $HOME/packages
-## radare2 (Manual installation)
+mkdir ${HOME}/packages
+cd ${HOME}/packages
 git clone https://github.com/radare/radare2.git
 ./radare2/sys/install.sh
-
+cd $HOME
 
 # Install oh-my-zsh and copy config files
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -pf ${DOTFILES}/.zshrc $HOME
-cp -pf ${DOTFILES}/.tmux.conf $HOME
-cp -pf ${DOTFILES}/.vimrc $HOME
+cp -pf ${HOME}/.dotfiles/.zshrc ${HOME}
+cp -pf ${HOME}/.dotfiles/.tmux.conf ${HOME}
+cp -pf ${HOME}/.dotfiles/.vimrc ${HOME}
 
 # Setup and update vim
 #vim -c 'PlugInstall'
