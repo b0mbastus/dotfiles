08f99231e589e9106cbbd3c5c873e78ebd7fc4c9
Jan Hanke <haja1023@hs-karlsruhe.de>
2018-06-08 09:01:54 +0000

    Further drafting of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 # Setup Script Draft
 
+# Make driectory to operate from
+# Change into directory
+# Clone dotfiles and copy everything into place
+# Run setup.sh
+
+###### SCRIPT ######
+
 # Create user
 # Set password
 # Add user to sudo group
 
 # Change to user
+
+# Add users-ssh key to authorized
 
 # Update and upgrade apt
 
@@ -21,12 +30,16 @@
 ## python
 ## g++
 ## make
-## radare2 (Manual installation)
 ## gdb
 ## nmap
+
+# Make directory for manually updated packages
+# Install manually updated packages
+## radare2 (Manual installation)
 
 # Setup git
 ## user.email
 ## user.name
 
-# Clone dotfiles and copy everything into place
+
+# Setup and update vim
