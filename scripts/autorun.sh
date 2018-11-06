0ab5454671fcf76aea6dd31f6bdbe9ccd2718397
Jan Hanke <jan-manuelhanke@gmx.de>
2018-11-06 22:00:39 +0100

    Modify autorun.sh; Add resources to dotfiles repo

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source ${HOME}/system.sh
+
 function run {
 	if ! pgrep $1;
 	then
@@ -9,3 +11,7 @@ function run {
 run compton
 run nm-applet
 run conky -p 1 -c ~/.config/conky/conky.conf
+
+if [[ $SYSTEM == "Laptop" ]]; then
+	run cbatticon
+fi
